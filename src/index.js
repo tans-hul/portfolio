a3b1444b9996ed6de6a0a8bf76fbad5c2e062398
tans-hul <tansul2469.be20@chitkara.edu.in>
2022-11-17 15:11:05 +0530

    amc

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
   // <React.StrictMode>
-    <Router>
-      <App />
-    </Router>,
+  <Router path='/'>
+    <App />
+  </Router>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
